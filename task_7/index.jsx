8926b0133a29102aa24f29ecb0c3236523010081
Искандер <42322349+Mirkadoi@users.noreply.github.com>
2022-05-25 00:05:06 +0300

    Update index.jsx

--- task_7/index.jsx
+++ task_7/index.jsx
@@ -1,31 +1,70 @@
 import { useState } from 'react';
+////////////////
+//Первый вариант
+////////////////
+
+const useBlockMouseEnter = (mouseEnterCallbak) => {
+  const [isActive, setActive] = useState(false);
+
+  const mouseEnterHandler = () => {
+    setActive(true);
+    mouseEnterCallbak();
+  };
+
+  return [isActive, mouseEnterHandler]
+}
 
 const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => {
-    const [ isActive, setActive ] = useState(false);
+  const [isActive, mouseEnterHandler] = useBlockMouseEnter(mouseEnterCallbak);
 
-    const mouseEnterHandler = () => {
-        setActive(true);
-        mouseEnterCallbak();
-    }
-
-    return (
-        <div onMouseEnter={mouseEnterHandler} className={ isActive ? 'active': '' }>
-            <img src={imgSrc} alt={imgAlt} />
-        </div>
-    );
-}
+  return (
+    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+      <img src={imgSrc} alt={imgAlt} />
+    </div>
+  );
+};
 
 const Block2 = ({ mouseEnterCallbak, content }) => {
-    const [ isActive, setActive ] = useState(false);
+  const [isActive, mouseEnterHandler] = useBlockMouseEnter(mouseEnterCallbak);
 
-    const mouseEnterHandler = () => {
-        setActive(true);
-        mouseEnterCallbak();
-    }
+  return (
+    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+      <p>BLock2 content: {content}</p>
+    </div>
+  );
+};
 
-    return (
-        <div onMouseEnter={mouseEnterHandler} className={ isActive ? 'active': '' }>
-            <p>BLock2 content: {content}</p>
-        </div>
-    );
+////////////////
+//Второй вариант
+////////////////
+
+const Wrapper = ({children, mouseEnterCallbak}) => {
+  const [isActive, setActive] = useState(false);
+
+  const mouseEnterHandler = () => {
+    setActive(true);
+    mouseEnterCallbak();
+  };
+
+  return (
+    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
+      {children}
+    </div>
+  )
 }
+
+const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => {
+  return (
+    <Wrapper mouseEnterCallbak={mouseEnterCallbak}>
+      <img src={imgSrc} alt={imgAlt} />
+    </Wrapper>
+  );
+};
+
+const Block2 = ({ mouseEnterCallbak, content }) => {
+  return (
+    <Wrapper mouseEnterCallbak={mouseEnterCallbak}>
+      <p>BLock2 content: {content}</p>
+    </Wrapper>
+  );
+};
